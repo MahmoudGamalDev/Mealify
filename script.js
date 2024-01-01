6dcdc50bae1ea6f08552963f60a071cb9b73e41d
Mahmoud Gamal <Mahmoudgamalawad10@gmail.com>
2024-01-01 20:21:23 +0200

    active link function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let navIcon = document.querySelector("header .nav-icon");
 let sideNav = document.querySelector(".side-nav");
 let hideIcon = document.querySelector(".hide i");
 let waveImages = document.querySelectorAll(".chef-cards img:last-of-type");
+let navLinks = document.querySelectorAll("nav ul li");
 
 let isDarkMode = false;
 
@@ -20,8 +21,8 @@ darkModeIcon.addEventListener("click", () => {
     stylesheet.href = "./css/style.css";
     darkModeIcon.classList.replace("fa-sun", "fa-moon");
     for (let i = 0; i < waveImages.length; i++) {
-        waveImages[i].src = "./imgs/wave2.svg";
-      }
+      waveImages[i].src = "./imgs/wave2.svg";
+    }
   }
 });
 
@@ -29,9 +30,19 @@ navIcon.addEventListener("click", () => {
   sideNav.style.display = "block";
   sideNav.style.right = "0";
   sideNav.style.position = "fixed";
-  console.log(89)
+  console.log(89);
 });
 
 hideIcon.addEventListener("click", () => {
   sideNav.style.display = "none";
 });
+
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove("active");
+    }
+    navLinks[i].classList.add("active");
+  });
+}
